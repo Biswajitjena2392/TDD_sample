7e1680a851101ab6e17bcad93a57621d5cef004a
Biswajitjena2392 <78969473+Biswajitjena2392@users.noreply.github.com>
2023-08-16 08:21:36 +0530

    added nextline addition function

--- src/main/java/tddKata/StringCalculator.java
+++ src/main/java/tddKata/StringCalculator.java
@@ -1,16 +1,30 @@
 package tddKata;
 
 public class StringCalculator {
-
+    private static String nextLine="\n";
+    private static String comma=",";
+    private static String Or="|";
+    public static String calculateSum(String... str){
+        int sum=0;
+        String response;
+        for(String each:str){
+            response=calculateSum(each);
+            sum+=Integer.parseInt(response);
+        }
+        return String.valueOf(sum);
+    }
     public static String calculateSum(String str){
         if(str.equals(""))
             return "0";
-        String[] numbers= str.split(",");
+        int sum=add(str.split(nextLine+Or+comma));
+        return String.valueOf(sum);
+    }
+    private static int add(String[] numbers){
         int sum=0;
         for(String number:numbers){
             int num=Integer.parseInt(number);
             sum+=num;
         }
-        return String.valueOf(sum);
+        return sum;
     }
 }
--- src/test/java/tddKata/StringCalculatorTest.java
+++ src/test/java/tddKata/StringCalculatorTest.java
@@ -27,4 +27,14 @@ class StringCalculatorTest {
         String ans= StringCalculator.calculateSum("1,2,3");
         assertEquals("6",ans);
     }
+    @Test
+    void SumOfAnyNumberOfNumbers(){
+        String ans=StringCalculator.calculateSum("1","1,2,3","");
+        assertEquals("7",ans);
+    }
+    @Test
+    void SumOfNumbersSeparatedByNextLine(){
+        String ans=  StringCalculator.calculateSum("1\n20,30");
+        assertEquals("51",ans);
+    }
 }
